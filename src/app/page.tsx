ede528f3ec7588aa6e85b0776942225b3856ba57
vickyadri29 <vickyadri290501@gmail.com>
2023-10-30 13:31:29 +0800

    feat(auth): handle check authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-2 items-start">
       <h1>Hello!</h1>
       <p>Welcome to Homepage!</p>
-      <Link href="/profile" className="p-2 bg-gray-800 hover:bg-gray-900 border border-white rounded-sm">
-        Profile
-      </Link>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,14 @@
+'use client'
+
+import { redirect } from "next/navigation";
+import { UserAuth } from "../context/AuthContext";
+
 const Profile = () => {
+  const { user }: any = UserAuth();
+
+  if(!user) {
+    redirect('/')
+  }
   return (
     <div>
       <p>Profile</p>
